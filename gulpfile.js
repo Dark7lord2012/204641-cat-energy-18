5e199990e4ea122b60f0180a850f88260ed3a74e
Dark7lord <budda185@mail.ru>
2019-11-17 19:43:42 +0900

    добавил таск в галпфайл "copy"

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,18 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
 gulp.task("server", function () {
   server.init({
     server: "source/",
